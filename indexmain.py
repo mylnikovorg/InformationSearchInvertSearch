b5a24c154cf880fb7069532f0a92562e74dd8055
alex <alex@mylnikov.org>
2014-10-18 20:05:13 +0400

    index updated

--- indexmain.py
+++ indexmain.py
@@ -1,4 +1,5 @@
 import pickle
+import sys
 
 __author__ = 'root'
 import re
@@ -7,33 +8,45 @@ import os
 import pymorphy2
 import string
 
+print('Argument List:', str(sys.argv[1]))
 
 morph = pymorphy2.MorphAnalyzer()
-os.chdir("./dir")
-termList, docLists = [], []
+os.chdir(sys.argv[1])
+termList = dict()
 for file in glob.glob("*.txt"):
+    counter = 0
     fileone = open(file, 'r')
     for line in fileone:
+
+        line = re.sub(
+            r"[" + """!"#$%&'()*+,./:;<=>?@[\]^_`{|}~©«""" + string.ascii_letters + string.digits + "„”\\\\\ufeff]",
+            " ",
+            line)
         print(line)
-        line = re.sub(r"[" + string.punctuation + "„”]", "", line)
         for word in line.split():
             term = morph.parse(word)[0].normal_form
             if term in termList:
-                indexOfTerm = termList.index(term)
+                if file in termList[term]:
+                    termList[term][file].add(counter)
+                else:
+                    termList[term][file] = {counter, }
 
-                docLists[indexOfTerm].add(file)
+
+
             else:
-                termList.append(term)
-                docLists.append({file, })
+                termList[term] = {str(file): {counter, }, }
+            counter += 1
 
 os.chdir("../")
 # f = open('index.inv','wb')
 # f.write()
-l1 = list(zip(termList, docLists))
-with open('index.inv', 'wb') as f:
-    pickle.dump(l1, f)
+print(termList)
 
-print("The index successfully saved to file 'index.inv'")
+# l1 = list(zip(termList, docLists))
+with open(sys.argv[2], 'wb') as f:
+    pickle.dump(termList, f)
+
+print("The index successfully saved to file '" + sys.argv[2] + "'")
 # f.close() # you can omit
 # for i in range(len(termList)):
 # print(i,termList[i], docLists[i], type(docLists[i]))
